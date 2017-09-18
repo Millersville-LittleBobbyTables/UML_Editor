2e789766207eac2cddc413de0481118440ccfaa8
zrzuch <zzuch25@gmail.com>
2017-09-18 02:32:25 -0400

    Top Menu and Content Area with Button Limited Interface

--- Main.java
+++ Main.java
@@ -2,14 +2,18 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
  
 public class Main extends Application
 {   
     private Stage window;
-    private GridPane grid;
+    private BorderPane layout;
+    private GridPane topGrid;
+    private GridPane centerGrid;
     private int num_elements = 0;
 
     public static void main( String[] args ) 
@@ -21,25 +25,50 @@ public class Main extends Application
     public void start( Stage primaryStage ) 
     {
         window = primaryStage;
-        grid = new GridPane();
-        grid.setPadding( new Insets( 10, 10, 10, 10 ));
-        grid.setVgap( 8 );
-        grid.setHgap( 10 );
 
-        Button btn = new Button("Add Textbox");
-        GridPane.setConstraints( btn, 0, 0 );
-        btn.setOnAction( e ->
+        topGrid = new GridPane();
+        topGrid.setPadding( new Insets( 10, 10, 10, 10 ));
+        topGrid.setVgap( 8 );
+        topGrid.setHgap( 10 );
+
+        centerGrid = new GridPane();
+        centerGrid.setPadding( new Insets( 10, 10, 10, 10 ));
+        centerGrid.setVgap( 8 );
+        centerGrid.setHgap( 10 );
+
+        layout = new BorderPane();
+        layout.setTop( topGrid );
+        layout.setCenter( centerGrid );
+
+        Button addTextBoxButton = new Button("Add Textbox");
+        GridPane.setConstraints( addTextBoxButton, 0, 0 );
+        addTextBoxButton.setOnAction( e ->
         {
             TextField cell = new TextField();
-            GridPane.setConstraints( cell, num_elements % 6, (num_elements / 6) + 1 );
-            grid.getChildren().add( cell );
+            GridPane.setConstraints( cell, num_elements % 6, num_elements / 6 );
+            centerGrid.getChildren().add( cell );
             num_elements++;
         });
+
+        ChoiceBox<String> connectorSelector = new ChoiceBox<>();
+        connectorSelector.getItems().addAll("Dependency", "Association", 
+            "Generalization", "Aggregation");
+        connectorSelector.setValue("Association");
+        GridPane.setConstraints( connectorSelector, 1, 0 );
+
+        Button useConnectorButton = new Button("Use Connector");
+        GridPane.setConstraints( useConnectorButton, 2, 0 );
+        String currentConnector;
+        useConnectorButton.setOnAction( e -> 
+            System.out.println( "Use the " + 
+            connectorSelector.getValue() + " Connector"));
+
+        topGrid.getChildren().addAll( addTextBoxButton, connectorSelector, 
+            useConnectorButton );
         
-        grid.getChildren().add( btn );
-        Scene scene = new Scene( grid, 1200, 900 );
+        Scene scene = new Scene( layout, 1200, 900 );
         window.setTitle("UML Editor");
         window.setScene( scene );
-        window.show();  
+        window.show();
     }
 }
