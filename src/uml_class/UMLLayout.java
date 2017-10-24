fc2ef618edf4c1760c87098bab79398f958d57b2
zrzuch <zzuch25@gmail.com>
2017-10-23 20:32:32 -0400

    f

--- src/uml_class/UMLLayout.java
+++ src/uml_class/UMLLayout.java
@@ -1,6 +1,5 @@
 package uml_class;
 
-<<<<<<< HEAD
 import javafx.scene.layout.Pane;
 import javafx.scene.control.TextArea;
 import javafx.scene.shape.Rectangle;
@@ -60,7 +59,7 @@ public class UMLLayout
     */
     public void setX(double x)
     {
-        this->x = x;
+        this.x = x;
         setXInLayout();
     }
 
@@ -69,7 +68,7 @@ public class UMLLayout
     */
     public void setY(double y)
     {
-        this->y = y;
+        this.y = y;
         setYInLayout();
     }
 
@@ -78,8 +77,8 @@ public class UMLLayout
     */
     public void setPosition(double x, double y)
     {
-        this->x = x;
-        this->y = y;
+        this.x = x;
+        this.y = y;
 
         setXInLayout();
         setYInLayout();
@@ -272,53 +271,3 @@ public class UMLLayout
         addToLayout();
     }
 }
-=======
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
-
-public class UMLLayout
-    {
-        private double width = 100;
-        private double height = 200;
-
-        private VBox textStack;
-        private TextArea top;
-        private TextArea mid;
-        private TextArea btm;
-
-        public UMLLayout( )
-        {
-            init();
-        }
-
-        public UMLLayout( double width, double height )
-        {
-            this.width = width;
-            this.height = height;
-            init();
-        }
-
-        private void init()
-        {
-            top = new TextArea();
-            top.setPrefWidth( width );
-            top.setPrefHeight( height / 9 );
-
-            mid = new TextArea();
-            mid.setPrefWidth( width );
-            mid.setPrefHeight( 4 * height / 9 );
-
-            btm = new TextArea();
-            btm.setPrefWidth( width );
-            btm.setPrefHeight( 4 * height / 9 );
-
-            textStack = new VBox();
-            textStack.getChildren().addAll( top, mid, btm );
-        }
-
-        public VBox getUMLBox()
-        {
-            return textStack;
-        }
-    }
->>>>>>> d843012108ec8fa329af0077ad658dbf0727834e
